40c72582823816dc308c68bcc366eefabfb3054f
Goosy-Goose <selina.gong0406@gmail.com>
2024-04-29 03:40:43 -0400

    Added Assets
    Added fonts, json file for dialogue, temp sprites.
    Changed nodes in VisualNovel scene.
    Got storyNode generation working

--- Visual Novel/VisualNovel.gd
+++ Visual Novel/VisualNovel.gd
@@ -3,8 +3,13 @@ extends Node
 @onready var text_timer = $TextTimer
 @onready var dot_timer = $DotTimer
 @onready var talk_timer = $TalkTimer
-@onready var curr = $Story
-@onready var body = $Screen/BodyContainer/BodyText
+@onready var curr_story_node = $Story
+@onready var dialogue_text = $DialogueTextLabel
+
+
+var file = "res://Assets/kitsune_game_dialogue.json"
+var json_as_text = FileAccess.get_file_as_string(file)
+var dialogue_dict = JSON.parse_string(json_as_text)
 
 var char_index = 0
 var path_arr = [0]
@@ -12,51 +17,63 @@ var bb_num = 0
 var talking = false
 
 func _ready():
-	body.text = curr.text
+	#if dialogue_dict:
+		#print(dialogue_dict)
+	makeStoryNodes()
+	dialogue_text.text = curr_story_node.text
+	curr_story_node.jumpToNode = str("Story/line0")
+	
 
 func _process(delta):
 	if Input.is_action_just_pressed("Continue"):
-		newCurr(curr.jumpToNode)
+		newCurr(curr_story_node.jumpToNode)
 
 func newCurr(path):
-	curr = get_node(path)
-	path_arr.append(curr.name)  
+	curr_story_node = get_node(path)
+	path_arr.append(curr_story_node.name)  
 	char_index = 0
 	text_timer.start()
-	body.text = curr.text
-	print(curr.text)
-	body.visible_characters = char_index
-	print(body.visible_characters)
+	dialogue_text.text = curr_story_node.text
+	print(curr_story_node.text)
+	dialogue_text.visible_characters = char_index
+	print(dialogue_text.visible_characters)
 	bb_num = 0
-	
+
+
 func _on_text_timer_timeout():
-	if char_index+1 < body.text.length():
-		if body.text[char_index] == '[':
+	if char_index+1 < dialogue_text.text.length():
+		if dialogue_text.text[char_index] == '[':
 			var bb_count = 0
-			while body.text[char_index+bb_count] != ']':
+			while dialogue_text.text[char_index+bb_count] != ']':
 				bb_count += 1
 			bb_num += bb_count + 1
 			char_index += bb_count
-		if body.text[char_index] == '“':
+		if dialogue_text.text[char_index] == '“':
 			talking = true
-		elif body.text[char_index] == '.':
+		elif dialogue_text.text[char_index] == '.':
 			text_timer.stop()
 			if talking == false:
 				dot_timer.start()
 			else:
 				talk_timer.start()
-		elif body.text[char_index] == '”':
+		elif dialogue_text.text[char_index] == '”':
 			talking = false
 			text_timer.stop()
 			dot_timer.start()
 		char_index += 1
-		body.visible_characters = char_index-bb_num
+		dialogue_text.visible_characters = char_index-bb_num
 	else:
 		char_index += 1
-		body.visible_characters = body.text.length()
+		dialogue_text.visible_characters = dialogue_text.text.length()
 
 func _on_dot_timer_timeout():
 	text_timer.start()
 	
 func _on_talk_timer_timeout():
 	text_timer.start()
+
+func makeStoryNodes():
+	for i in range(0, dialogue_dict.size() - 1):
+		var story_node = StoryNode.new()
+		story_node.fill_node(i, dialogue_dict[i])
+		curr_story_node.add_child(story_node)
--- Visual Novel/story.gd
+++ Visual Novel/story.gd
@@ -5,3 +5,11 @@ class_name StoryNode
 @export_multiline var text:String
 @export var type:String
 @export var jumpToNode:String
+@export var speakingChar:String
+
+func fill_node(i, dialogue_dict_entry):
+	name = str("line", i)
+	speakingChar = dialogue_dict_entry["Character name"]
+	text = dialogue_dict_entry["Dialogue"]
+	jumpToNode = str("Story/line", i+1)
+	
