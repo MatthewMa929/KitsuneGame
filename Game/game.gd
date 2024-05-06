85a933c9237ffe47125101f8c2b5e30ba4dc518e
mm11762 <MatthewMa929@Gmail.com>
2024-05-06 14:11:53 -0400

    sounds

--- Game/game.gd
+++ Game/game.gd
@@ -2,6 +2,12 @@ extends Node
 
 @onready var puzzle = $Puzzle
 @onready var visual_novel = $VisualNovel
+@onready var morning = $Morning
+@onready var breeze = $Breeze
+@onready var background = morning
+
+func _ready():
+	background.play()
 
 func _on_visual_novel_switch_to_puzzle():
 	puzzle.map.new_puzzle()
@@ -9,6 +15,7 @@ func _on_visual_novel_switch_to_puzzle():
 	visual_novel.visible = false
 	puzzle.player.on_script = false
 	visual_novel.old_story_node = visual_novel.curr_story_node
+	background.stop()
 
 func _process(delta):
 	if Input.is_action_just_pressed("Skip"):
@@ -26,3 +33,6 @@ func _on_puzzle_player_won():
 	print('on_enemy_player_won')
 	visual_novel.visible = true
 	puzzle.player.on_script = true
+	puzzle.map.background.stop()
+	background = breeze
+	background.play()
